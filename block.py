bc98440654a281ebc450ec88431321ebac53ce1e
sotka <kdstsdk@gmail.com>
2022-03-26 16:24:22 +0200

    add block

--- block.py
+++ block.py
@@ -1,22 +1,45 @@
-
-import blockchain
+from Crypto.Hash import SHA
+import json, datetime
 
 
 
 
 class Block:
-	def __init__(self):
+	def __init__(self,prev_hash,last_index,capacity):
 		##set
 
-		#self.previousHash
-		#self.timestamp
-		#self.hash
-		#self.nonce
-		#self.listOfTransactions
+		self.index = last_index + 1
+		self.timestamp = datetime.datetime.now()
+		self.hash = None
+		self.previousHash = prev_hash
+		self.nonce = None
+		self.listOfTransactions = [] #must be of size = capacity
+		self.capacity = capacity
 	
-	def myHash:
-		#calculate self.hash
+	def make_hash(self):
+		h = SHA.new(json.dumps(self.block_to_dict(),sort_keys=True).encode()).hexdigest()
+		self.hash = h 
+		return h
+
+	def block_to_dict(self, include_nonce = True):
+		temp = {'index':self.index,
+				'timestamp':str(self.timestamp),
+				'previousHash':self.previousHash,
+				'nonce':self.nonce,
+				'transactions':self.listOfTransactions}
+		return temp
 
 
-	def add_transaction(transaction transaction, blockchain blockchain):
-		#add a transaction to the block
+	def add_transaction(self,transaction):
+		self.listOfTransactions.append(transaction)
+
+
+	#getters & setters
+	def get_hash(self):
+		return self.hash
+
+	def get_nonce(self):
+		return self.nonce 
+
+	def set_nonce(self,nonce):
+		self.nonce = nonce 
